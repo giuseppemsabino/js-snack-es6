a40681bc984ef87a6cc60e6e21a0e3ed2d816ba3
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-25 14:33:25 +0200

    first commit

---
+++ 01-snack/script.js
@@ -0,0 +1,13 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bicicles= [
+    {
+        name: kawasaki,
+        peso: 10
+    },
+    {
+        name: 
+    }]
