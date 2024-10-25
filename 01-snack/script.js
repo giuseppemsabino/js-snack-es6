8cde99540b2ab20ece7cd9586ec22b410b6bd81f
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-25 14:49:34 +0200

    01

--- 01-snack/script.js
+++ 01-snack/script.js
@@ -3,11 +3,27 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
-const bicicles= [
+const bicycles= [
     {
-        name: kawasaki,
-        peso: 10
+        name:"Kawasaki",
+        mass: 10
     },
     {
-        name: 
-    }]
+        name: "Trek",
+        mass: 11
+    },
+    {
+        name:"Scott",
+        mass: 9
+    }];
+
+    // console.log(bicycles[0].name)
+
+    let lightBike = bicycles[0];
+
+    for(let i = 0; i < bicycles.length; i++){
+        if(bicycles[i].mass < lightBike.mass){
+            lightBike = bicycles[i]
+        }
+    }
+   console.log(`la bici piu leggera delinventario e ${lightBike.name} ha un peso di ${lightBike.mass}Km`)
