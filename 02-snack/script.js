a9a1cf607999ce8b1e7952a4a252c0f6ec13ff3a
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-25 15:08:54 +0200

    02 teams

--- 02-snack/script.js
+++ 02-snack/script.js
@@ -1,4 +1,36 @@
-// Snack 1
-// Creare un array di oggetti:
-// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// Stampare a schermo la bici con peso minore.
+// Snack 2
+// Creare un array di oggetti di squadre di calcio.
+//  Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const footballTeams= [
+    {
+        name:"Stark",
+        ponits: 0,
+        fouls: 0
+    },
+    {
+        name:"Lannister",
+        ponits: 0,
+        fouls: 0
+    },
+    {
+        name:"Martell",
+        ponits: 0,
+        fouls: 0
+    }];
+
+    footballTeams.forEach(team =>{
+        team.ponits = Math.floor(Math.random()*10);
+        team.fouls = Math.floor(Math.random()*3);
+    });
+    console.log(footballTeams)
+
+    const resumeTeams= footballTeams.map(team => {
+        return team.name, team.fouls;
+    });
+    console.log()
+
+    
